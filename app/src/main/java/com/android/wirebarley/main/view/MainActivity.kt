1c20a3afbbc2119a9da848be4a8afe5567391210
kimcumjee <51234550+kimcumjee@users.noreply.github.com>
2022-02-17 01:41:02 +0900

    [Add]getTime
    시간 받아오는 기능 구현 완료
    xml 대대적 수정 필요

--- app/src/main/java/com/android/wirebarley/main/view/MainActivity.kt
+++ app/src/main/java/com/android/wirebarley/main/view/MainActivity.kt
@@ -1,19 +1,32 @@
 package com.android.wirebarley.main.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.android.wirebarley.R
 import com.android.wirebarley.databinding.ActivityMainBinding
 import com.android.wirebarley.main.viewModel.MainViewModel
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val viewModel = MainViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
         viewModel.getExchangeList()
+        getTime()
         viewModel.exchangeReceived.observe(this) {
             
         }
+    }
+    private fun getTime() {
+        val now = System.currentTimeMillis()
+        val formatter = SimpleDateFormat("yyyy-MM-dd hh::mm", Locale.getDefault())
+        val date = formatter.format(now)
+        val text = getText(R.string.조회시간).toString() + date
+        binding.tvTime.text = text
     }
 }
